3c4a48d0f0fe07212a8503426f5a6ac8f3ce554f
Charles Gao <1030587461@qq.com>
2024-03-12 21:04:33 +0800

    Fixed some bugs of the dataset

--- preprocess_sft/preprocess_mp_sft.py
+++ preprocess_sft/preprocess_mp_sft.py
@@ -82,7 +82,6 @@ Stage 3 Post-process of translations
 Stage 4: Build SFT dataset
 """
 system_prompt = "你是专业的音乐作曲家。你知晓世界上的各种风格的音乐并理解音乐的韵律、意境，知晓世界上所有音乐家的人物经历、作曲风格以及他们创作的歌曲，懂得赏析他们的艺术创作，热衷于音乐相关的文学和艺术史。你对音乐有着痴迷般的着迷，当有人向你提问时你会非常热衷于解答，尽你所能地发挥你的创作能力为人们作曲、提供作曲建议。你懂得用ABC谱来展示你创作的音乐。你的行为举止非常优雅、讲究礼节，表现得体。你会根据用户输入的问题、需求进行分析并发挥你的创作能力作曲。你精通音乐领域的所有知识，能够完成用户输入的要求、请求、问题。"
-system_prompt_en = 'You are a professional music composer. You know various styles of music in the world and understand the rhythm and artistic conception of music. You know the personal experiences, composition styles and songs of all musicians in the world, know how to appreciate their artistic creations, and are keen on music-related literature and literature. Art History. You are obsessed with music. When someone asks you a question, you will be very keen to answer it and do your best to use your creative ability to compose and provide composition suggestions for people. You know how to use ABC notation to present the music you create. You behave with great grace, etiquette and propriety. You will analyze the questions and needs input by users and use your creative ability to compose music. You are proficient in all knowledge in the field of music and can complete requests and questions entered by users.'
 with open('./translation.pkl','rb') as f:
     translation = pickle.load(f)
 final_dataset = []
@@ -92,26 +91,22 @@ for _ in range(3):
 limit = len(dataset_irishman) * 3 // 4
 for i, data in tqdm(enumerate(dataset_irishman)):
     if i < limit:
-        system_prompt_final = system_prompt
         input_prompt = translation.get(data['instruction'].replace('Human: ','').strip(), 
                                        data['instruction'].replace('Human: ','').strip())
     else:
-        system_prompt_final = system_prompt_en
         input_prompt = data['instruction'].replace('Human: ','').strip()
     input_prompt += f'\n{data["input"].replace("</s>","")}'
     output = data['output'].replace('</s>','').replace('Assistant: ','').strip()
     conversation = {
-        'system': system_prompt_final,
+        'system': system_prompt,
         'input': input_prompt,
         'output': output
     }
-    final_dataset.append({'conversation': conversation})
+    final_dataset.append([{'conversation': conversation}])
 
 random.shuffle(final_dataset)
 
-for i in range(len(final_dataset)):
-    if i % 5000 == 4999:
-        with open(f'./sft_dataset/dataset-shard-{i - 4999}-{i}.json','w',encoding='utf-8') as f:
-            json.dump(final_dataset[i - 4999: i + 1], f, ensure_ascii=False, indent=4)
+with open(f'./sft_dataset/sft-dataset.json','w',encoding='utf-8') as f:
+    json.dump(final_dataset, f, ensure_ascii=False, indent=4)
 
     
