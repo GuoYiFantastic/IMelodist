5a8f08ea29d430cb12012eb07d209d062f57c71b
PommesPeter <me@pommespeter.space>
2024-03-16 00:55:59 +0800

    updated dependency

--- chat/app.py
+++ chat/app.py
@@ -1,2 +1,10 @@
 import os
+
+# install dependency
+os.system("pip install midi2audio")
+os.system("pip install music21")
+os.system("pip install streamlit")
+os.system("pip install transformers")
+
+# run demo
 os.system("streamlit run chat/IMelodist_demo.py --server.address=0.0.0.0 --server.port 7860")
