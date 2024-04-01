164e1eee005de346172951acb32c03e0a34b7d27
SchweitzerGAO <1030587461@qq.com>
2024-04-01 13:59:57 +0800

    Add a new example

--- IMelodist-7B/src/irishman2json-sft.py
+++ IMelodist-7B/src/irishman2json-sft.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 import json
 import random
 from typing import Dict
@@ -12,9 +13,12 @@ Stage 0: Configuations
 # with open('./openai_key.txt','r') as f:
 #     OPENAI_KEY = f.readlines()[0].strip()
 
-dataset = load_dataset('m-a-p/MusicPile-sft',cache_dir='../musicpile-sft')
+# dataset = load_dataset('sander-wood/irishman',cache_dir='../irishman')['train']
+# dataset.to_json('./irishman-cre.json')
 
-dataset_irishman = dataset.filter(lambda example: 'irishman' in example['src'])['train']
+# dataset = load_dataset('m-a-p/musicpile',cache_dir='../musicpile')
+# dataset_irishman = dataset.filter(lambda example: 'irishman' in example['src'])['train']
+# dataset_irishman.to_json('./irishman-sft.json')
 # # print(len(dataset_irishman['train']))
 # instructions = set([dataset_irishman['train'][i]['instruction'].replace("Human: ",'').strip() for i in range(len(dataset_irishman['train']))])
 # instructions = list(instructions)
@@ -83,32 +87,32 @@ Stage 3 Post-process of translations
 """
 Stage 4: Build SFT dataset
 """
-system_prompt = "你是专业的音乐作曲家。你知晓世界上的各种风格的音乐并理解音乐的韵律、意境，知晓世界上所有音乐家的人物经历、作曲风格以及他们创作的歌曲，懂得赏析他们的艺术创作，热衷于音乐相关的文学和艺术史。你对音乐有着痴迷般的着迷，当有人向你提问时你会非常热衷于解答，尽你所能地发挥你的创作能力为人们作曲、提供作曲建议。你懂得用ABC谱来展示你创作的音乐。你的行为举止非常优雅、讲究礼节，表现得体。你会根据用户输入的问题、需求进行分析并发挥你的创作能力作曲。你精通音乐领域的所有知识，能够完成用户输入的要求、请求、问题。"
-with open('./translation.pkl','rb') as f:
-    translation = pickle.load(f)
-final_dataset = []
-for _ in range(3):
-    dataset_irishman.shuffle(seed=42)
+# system_prompt = "你是专业的音乐作曲家。你知晓世界上的各种风格的音乐并理解音乐的韵律、意境，知晓世界上所有音乐家的人物经历、作曲风格以及他们创作的歌曲，懂得赏析他们的艺术创作，热衷于音乐相关的文学和艺术史。你对音乐有着痴迷般的着迷，当有人向你提问时你会非常热衷于解答，尽你所能地发挥你的创作能力为人们作曲、提供作曲建议。你懂得用ABC谱来展示你创作的音乐。你的行为举止非常优雅、讲究礼节，表现得体。你会根据用户输入的问题、需求进行分析并发挥你的创作能力作曲。你精通音乐领域的所有知识，能够完成用户输入的要求、请求、问题。"
+# with open('./translation.pkl','rb') as f:
+#     translation = pickle.load(f)
+# final_dataset = []
+# for _ in range(3):
+#     dataset_irishman.shuffle(seed=42)
 
-limit = len(dataset_irishman) * 3 // 4
-for i, data in tqdm(enumerate(dataset_irishman)):
-    if i < limit:
-        input_prompt = translation.get(data['instruction'].replace('Human: ','').strip(), 
-                                       data['instruction'].replace('Human: ','').strip())
-    else:
-        input_prompt = data['instruction'].replace('Human: ','').strip()
-    input_prompt += f'\n{data["input"].replace("</s>","")}'
-    output = data['output'].replace('</s>','').replace('Assistant: ','').strip()
-    conversation = {
-        'system': system_prompt,
-        'input': input_prompt,
-        'output': output
-    }
-    final_dataset.append({'conversation': [conversation]})
+# limit = len(dataset_irishman) * 3 // 4
+# for i, data in tqdm(enumerate(dataset_irishman)):
+#     if i < limit:
+#         input_prompt = translation.get(data['instruction'].replace('Human: ','').strip(), 
+#                                        data['instruction'].replace('Human: ','').strip())
+#     else:
+#         input_prompt = data['instruction'].replace('Human: ','').strip()
+#     input_prompt += f'\n{data["input"].replace("</s>","")}'
+#     output = data['output'].replace('</s>','').replace('Assistant: ','').strip()
+#     conversation = {
+#         'system': system_prompt,
+#         'input': input_prompt,
+#         'output': output
+#     }
+#     final_dataset.append({'conversation': [conversation]})
 
-random.shuffle(final_dataset)
+# random.shuffle(final_dataset)
 
-with open('./sft_dataset/sft-dataset.json','w',encoding='utf-8') as f:
-    json.dump(final_dataset, f, ensure_ascii=False, indent=4)
+# with open('./sft_dataset/sft-dataset.json','w',encoding='utf-8') as f:
+#     json.dump(final_dataset, f, ensure_ascii=False, indent=4)
 
     
