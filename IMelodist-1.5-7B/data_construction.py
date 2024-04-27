0250a6eeac23826a402baa8c78d5d43bbcad120f
SchweitzerGAO <1030587461@qq.com>
2024-04-27 16:27:59 +0800

    [Data] add irishman dataset code

--- IMelodist-1.5-7B/data_construction.py
+++ IMelodist-1.5-7B/data_construction.py
@@ -1,16 +1,36 @@
 import os
-from datasets import Dataset, Audio
+from datasets import Dataset, Audio, load_dataset
 from transformers import EncodecModel, AutoProcessor
 from utils import encodec_to_anygpt_vec
 import json
 import random
 
-# Settings
-home = os.environ['HOME']
+# System and templates
 system = """I am a music AI assistant named IMelodist based on InternLM2. I have plenty knowledge of basic music theories and am capable of composing beautiful 
 music. I am also good at ABC notation which denotes music with pure ASCII characters. I can correct mistakes of an ABC notation score, generate ABC
 notation score according to the music given by user and compose music with ABC notation score as well.
 """
+abc_header_template = """Commonly used ABC notation header fields are:
+X: reference number
+L: unit note length
+Q: tempo
+M: meter
+K: key
+Below is a sample header:
+{sample_header}
+"""
+irishman_template = """Here is a sample of music denoted in ABC notation, I should pay attention to these points and their appearance in the sample:
+1. The format of a header;
+2. The denotation of notes;
+3. The denotation of bar lines(\'|\' or \'||\' or \'[|\' or \'|]\');
+4. The denotation of repetitions (\'::\' or between \'|:\' and \':|\');
+{abc_notation}
+"""
+
+# Settings
+home = os.environ['HOME']
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+os.environ['HF_HUB_ENABLE_HF_TRANSFER'] = '1'
 encodec_path = f'{home}/models/encodec_32khz'
 desc_path = f'{home}/IMelodist-1.5/IMelodist-1.5-7B/music_part2.json'
 any_instruct_dataset_path = f'{home}/music-data/part2/music'
@@ -72,52 +92,58 @@ Any Instruct
 #     json.dump(pt_dataset,f, ensure_ascii=False,indent=4)
 
 """
-ABC Header
+ABC header
 """
 
-abc_header_template = """Commonly used ABC notation header fields are:
-X: reference number
-L: unit note length
-Q: tempo
-M: meter
-K: key
-Below is a sample header:
-{sample_header}
-"""
+# unit_note_lens = [f'1/{2 ** i}' for i in range(1, 5)]
+# meters = ['2/2','4/4','3/4','6/8','3/8','5/4','9/8','12/8']
+# keys = ['A','Am','Ab','Abm','B','Bm','Bb','Bbm','C','Cm','C#','C#m','D','Dm','E','Em','Eb','Ebm','F','Fm','F#','F#m','G','Gm']
 
-unit_note_lens = [f'1/{2 ** i}' for i in range(1, 5)]
-meters = ['2/2','4/4','3/4','6/8','3/8','5/4','9/8','12/8']
-keys = ['A','Am','Ab','Abm','B','Bm','Bb','Bbm','C','Cm','C#','C#m','D','Dm','E','Em','Eb','Ebm','F','Fm','F#','F#m','G','Gm']
+# def sample_header_generate(n: int):
+#     data = []
+#     for _ in range(n):
+#         has_tempo = bool(random.getrandbits(1))
+#         ref = random.randint(1, 256)
+#         tempo = random.choice([f'1/4={random.randint(96, 200)}',f'1/8={random.randint(96, 200)}',f'3/8={random.randint(96, 200)}'])
+#         unl = random.choice(unit_note_lens)
+#         meter = random.choice(meters)
+#         key = random.choice(keys)
+#         header = f'X:{ref}\nL:{unl}\n'
+#         if has_tempo:
+#             header += f'Q:{tempo}\n'
+#         header += f'M:{meter}\nK:{key}'
+#         train_data = system + abc_header_template.replace('{sample_header}',header) 
+#         data.append({
+#             'conversation': [
+#                 {
+#                     'system':"",
+#                     'input': "",
+#                     'output': train_data
+#                 }
+#             ]
+#         })
+#     with open('./dataset_abc_header.json','w',encoding='utf-8') as f:
+#         json.dump(data, f, ensure_ascii=False, indent=4)
 
-def sample_header_generate(n: int):
-    data = []
-    for _ in range(n):
-        has_tempo = bool(random.getrandbits(1))
-        ref = random.randint(1, 256)
-        tempo = random.choice([f'1/4={random.randint(96, 200)}',f'1/8={random.randint(96, 200)}',f'3/8={random.randint(96, 200)}'])
-        unl = random.choice(unit_note_lens)
-        meter = random.choice(meters)
-        key = random.choice(keys)
-        header = f'X:{ref}\nL:{unl}\n'
-        if has_tempo:
-            header += f'Q:{tempo}\n'
-        header += f'M:{meter}\nK:{key}'
-        train_data = system + abc_header_template.replace('{sample_header}',header) 
-        data.append({
-            'conversation': [
-                {
-                    'system':"",
-                    'input': "",
-                    'output': train_data
-                }
-            ]
-        })
-    with open('./dataset_abc_header.json','w',encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-sample_header_generate(30000)
+# sample_header_generate(30000)
 
 
 """
 Irishman
 """
+irishman_dataset = load_dataset('sander-wood/irishman',split='train',cache_dir=irishman_dataset_path)
+irishman_data = []
+for d in irishman_dataset:
+    train_data = system + '\n' + irishman_template.replace('{abc_notation}',d['abc notation'])
+    irishman_data.append({
+        'conversation':[
+            {
+                'system':'',
+                'input': '',
+                'output': train_data
+            }
+        ]
+    })
+random.shuffle(irishman_data)
+with open('./datasets/dataset_irishman.json','w',encoding='utf-8') as f:
+    json.dump(irishman_data, f, ensure_ascii=False, indent=4)
